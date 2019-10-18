f592307f101a09848bb50c1bcbb4602eb52a9c8a
nakano1120 <38690180+nakano1120@users.noreply.github.com>
2019-10-18 15:49:18 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let play=0///今動いているか
 let count=1///アニメのカウント
 let dokaben = document.getElementById("kadoben");///動作をする要素
+
 function dokabenscript(){
     if(play>0){///もしプレイしていたら
         return;///やらせない（固い意志）
@@ -8,18 +9,21 @@ function dokabenscript(){
 
     play=1///動いていることにする
 
-    let obj = document.getElementsByClassName("dokaben");///
-    dokaben.innerHTML=document.getElementById("dokabentext").value;
-    count = 0;
-    let i=9
-    dokaben.id=("ani0")
+    dokaben.innerHTML=document.getElementById("dokabentext").value;///入力された文字を反映
+
+    count = 0;///カウントをゼロに
+    dokaben.id=("ani0")///アニメをゼロに戻す
+
     let intarval = setInterval(function(){
-        count++;
-        dokaben.id=("ani"+count);
-        if(count >= 9){
-            clearInterval(intarval);　//idをclearIntervalで指定している
-            play=0
+
+        count++;///カウントしていく
+        dokaben.id=("ani"+count);///IDを書き換え
+
+        if(count >= 9){///アニメが終了したら
+            clearInterval(intarval);　//intervalを終わる
+            play=0///動いていないことにする
         }
+
     },200)
     
 }
